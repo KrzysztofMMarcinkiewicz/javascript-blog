01812535a3904cc07163fd52810e9aba914c9d1a
Krzysztof Marcinkiewicz <marcinkiewiczkrzysiek@gmail.com>
2023-05-24 11:09:07 +0200

    Changes in all files

--- js/script.js
+++ js/script.js
@@ -10,6 +10,12 @@ function titleClickHandler(event){
 
   /* remove class 'active' from all articles */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }   
+
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
